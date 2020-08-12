bd311a7cb9baa1b5824e2ea80b89ac356eb31b49
MONSTER_HACK <69053807+MONSTER-hp@users.noreply.github.com>
2020-08-12 10:08:46 +0430

    Update Monster_WebCam.sh

--- Monster_WebCam.sh
+++ Monster_WebCam.sh
@@ -1,21 +1,14 @@
-#!/bin/bash
-# SayCheese v1.0
-# coded by: github.com/HACKGM/hackwebcam
-# If you use any part from this code, giving me the credits. Read the Lincense!
+
+
+# HACKED BY MONSTER_hp
 
 trap 'printf "\n";stop' 2
 
 banner() {
 
-printf "\e[1;92m       ⁪⁬⁮⁮⁮⁮         ⁪⁬⁮⁮⁮⁮   \e[0m\e"
-
-
-printf "\e[1;92m       ⁪⁬⁮⁮⁮⁮         ⁪⁬⁮⁮⁮⁮   \e[0m\e"
-
-printf "\e[1;92m   𝙃𝘼𝘾𝙆 𝙒𝙀𝘽𝘾𝘼𝙈 📸\e[0m\e   \e[0m\n"
-printf "\e[1;92m   Canal telegram: T.me/HACKGM\e[0m                                 \n"
-
-printf " \e[1;77m v1.0 coded by https://github.com/HACKGM/cam\e[0m \n"
+printf "\e[91m    Hack Copy Type Target !!! \n"
+printf " \e[1;94m GitHub : https://github.com/MONSTER-hp/MONSTER_HACK \n"
+printf " \e[1;92m Canell : https://t.me/MONSTER_SECURITY\n"
 
 printf "\n"
 
